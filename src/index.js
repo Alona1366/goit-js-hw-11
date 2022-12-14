e9a6e758bfbf606d362cff4bb2ab2961e905967a
Alona Ihnatchenko <olena1366@gmail.com>
2022-12-14 23:31:54 +0200

    update index js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // import { fetchImagesApi } from './fetchImagesApi';
+
+const { searchForm, imageGallery, guard } = {
+    searchForm: document.querySelector('#search-form'),
+    imageGallery: document.querySelector('.gallery'),
+    guard: document.querySelector('.js-guard'),
+  };
+
+  const simpleligthbox = new SimpleLightbox('.gallery a', { loop: false });
